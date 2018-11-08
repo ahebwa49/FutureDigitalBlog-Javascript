4e9f8670f0c025c330fc274002361796123ae8b1
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-08 15:37:54 +0300

    Add a label to renderField function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -7,6 +7,7 @@ class PostsNew extends Component{
     renderField(field){
       return(
         <div className="form-group">
+        <label>{field.label}</label>
           <input
             className="form-control"
             type="text"
