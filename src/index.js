feea02042c1e8540250f65dea9c4b7a3e69369aa
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-08 15:48:59 +0300

    import PostsNew

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 import reducers from './reducers';
 import ReduxPromise from 'redux-promise';
