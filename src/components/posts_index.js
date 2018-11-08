b86ec679d19b784d0c52ea5bbe649f1efb83cdc5
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-08 11:36:59 +0300

    return a div

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 
 class PostsIndex extends Component{
   render(){
-    
+    return(
+      <div>
+        <h3>PostsIndex</h3>
+      </div>
+    );
   }
 }
