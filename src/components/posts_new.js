d41cda63cf9b1ca95d2881d1d6141c30e2cc70fe
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-09 13:24:55 +0300

    import createPost action creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {createPost} from '../actions/index';
 
 
 class PostsNew extends Component{
