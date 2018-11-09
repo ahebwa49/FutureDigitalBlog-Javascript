8dfbaeeec3aeec13a2753d35429169da1b57b29c
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-09 13:22:20 +0300

    import connect

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 
 class PostsNew extends Component{
